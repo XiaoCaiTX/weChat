0f47d70e469cb86e0c11e85cbf7144a855cbdbf0
kc <kc@163.com>
2016-03-12 09:36:56 +0800

    添加了一条🐶

---
+++ weChat/weChat/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  weChat
+//
+//  Created by Kevin on 16/3/12.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ weChat/weChat/Dog.m
@@ -0,0 +1,14 @@
+
+//
+//  Dog.m
+//  weChat
+//
+//  Created by Kevin on 16/3/12.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
