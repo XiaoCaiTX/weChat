e651a1f797aec796dd3b83f68e180cdc81900d26
kc <kc@163.com>
2016-03-12 09:34:46 +0800

    初始化项目

---
+++ weChat/weChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weChat
+//
+//  Created by Kevin on 16/3/12.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
